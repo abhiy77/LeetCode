731a216952082adc7ddda16af3069419806226f2
Abhishek Yelisetti <42091343+abhiy77@users.noreply.github.com>
2020-09-05 14:26:39 +0530

    Palindrome Number

--- LeetCode 1-100/9. PalindromeNumber.java
+++ LeetCode 1-100/9. PalindromeNumber.java
@@ -1,4 +1,5 @@
 class Solution {
+    
     public boolean isPalindrome(int x) {
         if(x < 0 || x!=0 && x%10 == 0) return false;
         
@@ -10,4 +11,5 @@ class Solution {
         }
         return x == half_num || x == half_num/10; 
     }
+    
 }
