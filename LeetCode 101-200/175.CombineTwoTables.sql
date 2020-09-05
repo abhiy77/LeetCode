2a622b583b08a14bb182d1c94dc98858f8a19cad
Abhishek Yelisetti <42091343+abhiy77@users.noreply.github.com>
2020-09-05 13:38:22 +0530

    Combine Two Tables

--- LeetCode 101-200/175.CombineTwoTables.sql
+++ LeetCode 101-200/175.CombineTwoTables.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
 
-select FirstName,LastName,City,State from Person 
-        left join Address on Person.PersonId = Address.PersonId; 
+select 
+      FirstName,LastName,City,State 
+from 
+      Person 
+        left join 
+      Address 
+on      
+      Person.PersonId = Address.PersonId; 
