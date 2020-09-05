c62b47e276d306748ff4b5631ec21a6e5fe53b48
Abhishek Yelisetti <42091343+abhiy77@users.noreply.github.com>
2020-09-05 13:51:00 +0530

    Divide Two Integers

--- LeetCode 1-100/29. DivideTwoIntegers.java
+++ LeetCode 1-100/29. DivideTwoIntegers.java
@@ -1,4 +1,5 @@
 class Solution {
+    
     public int divide(int dividend, int divisor) {
         if (dividend == Integer.MIN_VALUE && divisor == -1) 
             return Integer.MAX_VALUE;
@@ -6,4 +7,5 @@ class Solution {
             return (dividend == Integer.MIN_VALUE)? 1 : 0;
         return (int)Math.floor(dividend/divisor);
     }
+    
 }
