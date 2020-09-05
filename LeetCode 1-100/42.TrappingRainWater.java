5f66565a54676c1bafd70d719dbd921faa15a563
Abhishek Yelisetti <42091343+abhiy77@users.noreply.github.com>
2020-09-05 13:59:18 +0530

    Trapping Rain Water

--- LeetCode 1-100/42.TrappingRainWater.java
+++ LeetCode 1-100/42.TrappingRainWater.java
@@ -1,4 +1,5 @@
 class Solution {
+    
     public int trap(int[] height) {
         if(height == null || height.length == 0) return 0;
         
@@ -20,4 +21,5 @@ class Solution {
         }
         return water;
     }
+    
 }
