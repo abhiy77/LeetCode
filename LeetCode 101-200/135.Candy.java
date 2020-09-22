666a37d272b1b1d74456041d8723a54d558ceb0b
Abhishek Yelisetti <42091343+abhiy77@users.noreply.github.com>
2020-09-22 14:58:07 +0530

    Candy

---
+++ LeetCode 101-200/135.Candy.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int candy(int[] ratings) {
+        if(ratings.length == 0) return 0;
+        int candies = 0;
+        int n = ratings.length;
+        int left[] = new int[n];
+        int right[] = new int[n];
+        Arrays.fill(left,1);
+        Arrays.fill(right,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1] + 1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]){
+                right[i] = right[i+1] + 1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            candies += Math.max(left[i],right[i]);
+        }
+        return candies;
+    }
+}
