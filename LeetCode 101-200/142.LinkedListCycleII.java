25ef31a30aaea5459f813f5eb1b2d037dacb6859
Abhishek Yelisetti <42091343+abhiy77@users.noreply.github.com>
2020-09-22 18:09:40 +0530

    Linked List Cycle II

---
+++ LeetCode 101-200/142.LinkedListCycleII.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+        if(head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        do{
+            fast = fast.next.next;
+            slow = slow.next;
+
+        }while(fast != null && fast.next != null && slow != fast);
+        if(slow != fast) return null;
+        else{
+            slow = head;
+            
+            while(slow != fast){
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+}
