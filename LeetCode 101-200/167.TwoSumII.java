9a01ee5a896536254e216eb0baba748313bb7f1c
Abhishek Yelisetti <42091343+abhiy77@users.noreply.github.com>
2020-09-23 13:06:18 +0530

    Two Sum II

---
+++ LeetCode 101-200/167.TwoSumII.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans = new int[2];
+        if(numbers.length <= 1)return ans;
+        int i = 0;
+        int j = numbers.length - 1;
+        while(i < j){
+            if(numbers[i] + numbers[j] == target){
+                ans[0] = i+1;
+                ans[1] = j+1;
+                return ans;
+            }
+            else if(numbers[i] + numbers[j] < target){
+                i++;
+            }
+            else j--;
+        }
+        return ans;
+    }
+}
